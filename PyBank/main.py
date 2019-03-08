ed4d4ed1ccfd98a2f647de1bad39f5bff16f1d81
sgreyna <47375305+sgreyna@users.noreply.github.com>
2019-03-07 18:34:30 -0800

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,37 +11,53 @@ with open(file_csv, newline='') as csvfile:
 	#Read the header row
 	csv_header = next(csvfile)
 	
-	#Read through each row
-	for row in csvreader:
+	#Get the value of firstRow ans assign to varriable
+	firstRow=[] 
+	firstRow=next(csvreader)
+	maxMonth = firstRow[0]
+	maxValue = firstRow[1]
 	
-	#The total number of months included in the dataset
-		maxNum = 0
-		minNum = 0
-		totalMonths = 0
-		totalNum = 0
+	#Read through each row	
+	cntMonths = 0
+	totalNum = 0
+	for row in csvreader:
+		if len(row[0]) >1 :  #for value in column have more than 1 character
+			cntMonths += 1 #cnt each row as month
+			totalNum += int(row[1]) #add each month values
+			
+		if float(row[1]) > float(maxValue):  #if value is more than first value 
+			maxValue = float(row[1]) #then replace the value
+			maxMonth = row[0] #replace the month
 		
-		
-		if row[0] !="":
-			totalMonths += 1
-			totalNum += float(row[1])
-					
-		#if float(row[1]) > maxNum:
-		#	maxNum = float=([row[1])
-		#	maxMonth = row[0]
-		#	print (MaxNum)
-					
-		#if float(row[1]) < minNum:
-		#	minNum = float(row[1])
-		#	minMonth = row[0]
-
-		#AvgNum = TotalNum / TotalMonths	
-				
-	#Print to console and export to Excel				
+		if float(row[1]) < float(maxValue):  #if value is more than less value 
+			minValue = float(row[1]) #then replace the value
+			minMonth = row[0] #replace the month
+	
+	avgChange = totalNum/cntMonths	
+	
+	#Print to console and export to Excel	
+	print("")			
 	print ("Financial Analysis")
 	print("")
 	print ("---------------------------------------------------------")
-	print(f" totalMonths: " + totalMonths)
-
-		
-		
-		
+	print(f'Total Months:   {cntMonths}')
+	print(f'Total:   ${totalNum}')
+	print(f'Average  Change:   ${round(avgChange,2)}')
+	print(f'Greatest Increase in Profits:   {maxMonth}  ${maxValue}')
+	print(f'Greatest Decrease in Profits:  {minMonth}  ${minValue}')
+	
+# Specify the file to write to
+output_path = os.path.join("..", "resources", "summary.csv")
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline='') as newcsvfile:
+	# Initialize csv.writer
+	newcsvwriter = csv.writer(newcsvfile)
+    	#Write the report header
+	newcsvwriter.writerow(['Financial Analysis'])		
+	# Write the all other row
+	newcsvwriter.writerow(['---------------------------------------------------------'])
+	newcsvwriter.writerow(['Total Months:', cntMonths])
+	newcsvwriter.writerow(['Total:', 'Frost', totalNum])
+	newcsvwriter.writerow(['Average Change:', avgChange])
+	newcsvwriter.writerow(['Greatest Increase in Profits: ', maxMonth, maxValue])
+	newcsvwriter.writerow(['Greatest Increase in Profits: ', minMonth, minValue])
